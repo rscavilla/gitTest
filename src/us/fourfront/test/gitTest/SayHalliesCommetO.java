e2b60b6ed89e81c71e58d36dd0974c551c69323e
rscavilla <rscavilla@gmail.com>
2016-12-30 15:30:49 -0500

    Commit message for local Gitr repository

---
+++ src/us/fourfront/test/gitTest/SayHalliesCommetO.java
@@ -0,0 +1,17 @@
+package us.fourfront.test.gitTest;
+
+public class SayHalliesCommetO {
+
+	public SayHalliesCommetO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		String greeting = "Hallies Commet O";
+		sayIt(greeting);
+	}
+
+	private static void sayIt(String msg) {// testing SVN
+		System.out.println(msg);
+	}
+}
